23c7b23ecb7b32b39ce547305e5e0398262cd045
JacobL260 <jal260@uakron.edu>
2026-03-04 18:28:04 -0500

    new test

--- Tests/motor_test.py
+++ Tests/motor_test.py
@@ -1,31 +1,71 @@
 import lgpio
-import time
+from time import sleep
 
-STEP_PIN = 5
-DIR_PIN = 6
+# Direction pin from controller
+DIR = 6
 
-# open gpio chip
+# Step pin from controller
+STEP = 5
+
+# 0/1 used to signify clockwise or counterclockwise
+CW = 1
+CCW = 0
+
+steps = 200        # 200 steps = 360 degrees (depends on driver)
+speed = 0.0005     # delay between steps
+
+# Open GPIO chip
 h = lgpio.gpiochip_open(0)
 
-# claim pins as outputs
-lgpio.gpio_claim_output(h, STEP_PIN)
-lgpio.gpio_claim_output(h, DIR_PIN)
+# Claim pins as outputs
+lgpio.gpio_claim_output(h, DIR)
+lgpio.gpio_claim_output(h, STEP)
 
-# set direction
-lgpio.gpio_write(h, DIR_PIN, 1)   # 1 = clockwise, 0 = counterclockwise
+# Set initial direction
+lgpio.gpio_write(h, DIR, CW)
 
-steps = 20000000000
-delay = 0.01   # controls speed
+try:
+    # Run forever
+    while True:
 
-print("Running stepper...")
+        """
+        Change Direction: Changing direction requires time to switch.
+        The time is dictated by the stepper motor and controller.
+        """
 
-for i in range(steps):
-    lgpio.gpio_write(h, STEP_PIN, 1)
-    time.sleep(delay)
-    lgpio.gpio_write(h, STEP_PIN, 0)
-    time.sleep(delay)
+        sleep(1.0)
 
-print("Done")
+        # Set clockwise direction
+        lgpio.gpio_write(h, DIR, CW)
 
-# close gpio chip
-lgpio.gpiochip_close(h)
+        # Run for 200 steps
+        for x in range(steps):
+
+            # Set step high
+            lgpio.gpio_write(h, STEP, 1)
+
+            # Motor speed control
+            sleep(speed)
+
+            # Set step low
+            lgpio.gpio_write(h, STEP, 0)
+
+            sleep(speed)
+
+        sleep(1.0)
+
+        # Change direction
+        lgpio.gpio_write(h, DIR, CCW)
+
+        for x in range(steps):
+
+            lgpio.gpio_write(h, STEP, 1)
+            sleep(speed)
+
+            lgpio.gpio_write(h, STEP, 0)
+            sleep(speed)
+
+# Cleanup on CTRL+C
+except KeyboardInterrupt:
+    print("cleanup")
+    lgpio.gpiochip_close(h)
